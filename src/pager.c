9b0ea45c2e883115b125640b7921817077c3a59e
Pen4o1 <pivv06iwo@gmail.com>
2026-03-31 23:55:32 +0300

    fixed some small things

--- src/pager.c
+++ src/pager.c
@@ -8,18 +8,20 @@ void* get_page(struct Pager* pager, uint32_t page_num)
 {
     if (page_num > TABLE_MAX_PAGES) return;
 
-    if (pager->pages[page_num] == NULL) {
+    if (pager->pages[page_num] == NULL) 
+    {
         void* page = malloc(PAGE_SIZE);
         uint32_t num_pages = pager->file_lenght / PAGE_SIZE;
 
-        if (pager->file_lenght % PAGE_SIZE) {
+        if (pager->file_lenght % PAGE_SIZE)
             num_pages += 1;
-        }
 
-        if (page_num <= num_pages) {
+        if (page_num <= num_pages) 
+        {
             lseek(pager->file, page_num * PAGE_SIZE, SEEK_SET);
             ssize_t bytes_read = read(pager->file, page, PAGE_SIZE);
-            if (bytes_read == -1) {
+            if (bytes_read == -1) 
+            {
                 printf("Error reading file: %d\n", errno);
                 exit(EXIT_FAILURE);
             }
