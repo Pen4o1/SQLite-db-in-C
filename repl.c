c7bec2c50599ba9d0d32ba30735cf3f4250527be
Pen4o1 <pivv06iwo@gmail.com>
2026-03-31 15:49:23 +0300

    added a check for the number of args

--- repl.c
+++ repl.c
@@ -4,6 +4,23 @@
 #include "table.h"
 #include "row.h"
 
+
+int numOfArgs(char* str)
+{
+    int count = 0;
+
+    char tempStr[1024];
+    strcpy(tempStr, str);
+
+    char* token = strtok(tempStr, " ");
+    while(token != NULL)
+    {
+        count+=1;
+        token = strtok(NULL, " ");
+    }
+    return count - 1;
+}
+
 void repl(struct Table* table)
 {
     char input[1024];
@@ -20,10 +37,19 @@ void repl(struct Table* table)
         {
             struct Row row;
             printf("Eneter the id, name and email \n");
-            sscanf(input, "insert %d %s %s", &row.id, row.userName, row.email);
+            ifsscanf(input, "insert %d %s %s", &row.id, row.userName, row.email);
 
-            insert_row(table, row);
-            printf("Executed.\n");
+            int countArgs = numOfArgs(input);
+
+            if(countArgs > 3)
+                printf("Too many args\n");
+            else if (countArgs < 3)
+                printf("Too few args\n");
+            if(countArgs == 3)
+            {
+                insert_row(table, row);
+                printf("Executed.\n");
+            }
         }
         else if (strcmp(input, "select") == 0)
             select_rows(table);
